947dfb184486db0c5ef6e92fd5450512ab90f62d
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2026-01-25 01:24:35 +0700

    Add Transaction class for managing transaction details

---
+++ src/main/java/com/cursebyte/plugin/economy/Transaction.java
@@ -0,0 +1,37 @@
+package com.cursebyte.plugin.economy;
+
+public class Transaction {
+    private final String sender;
+    private final String receiver;
+    private final double amount;
+    private final long time;
+    private final String description;
+
+    public Transaction(String sender, String receiver, double amount, long time, String description) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.amount = amount;
+        this.time = time;
+        this.description = description;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
