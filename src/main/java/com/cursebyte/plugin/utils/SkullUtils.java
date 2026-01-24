ebe37e0113fcfa393278efbb27fc220c593c00dc
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2026-01-25 01:26:20 +0700

    Add SkullUtils class for creating custom player heads from base64 textures

---
+++ src/main/java/com/cursebyte/plugin/utils/SkullUtils.java
@@ -0,0 +1,27 @@
+package com.cursebyte.plugin.utils;
+
+import com.destroystokyo.paper.profile.PlayerProfile;
+import com.destroystokyo.paper.profile.ProfileProperty;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+
+import java.util.UUID;
+
+public class SkullUtils {
+
+    public static ItemStack getCustomHead(String base64) {
+        ItemStack head = new ItemStack(Material.PLAYER_HEAD);
+        if (base64 == null || base64.isEmpty()) return head;
+
+        SkullMeta meta = (SkullMeta) head.getItemMeta();
+        PlayerProfile profile = Bukkit.createProfile(UUID.randomUUID());
+        
+        profile.setProperty(new ProfileProperty("textures", base64));
+        meta.setPlayerProfile(profile);
+        head.setItemMeta(meta);
+        
+        return head;
+    }
+}
