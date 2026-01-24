38cf8c33b0e9f4e4543821e24b4ac39537bbb767
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2026-01-25 01:25:04 +0700

    Add TransactionListener for handling player interactions and initiating transfers

---
+++ src/main/java/com/cursebyte/plugin/listener/TransactionListener.java
@@ -0,0 +1,26 @@
+package com.cursebyte.plugin.listener;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.EquipmentSlot;
+
+import com.cursebyte.plugin.ui.core.MenuContext;
+import com.cursebyte.plugin.ui.core.MenuRouter;
+
+public class TransactionListener implements Listener {
+    @EventHandler
+    public void onRightClickPlayer(PlayerInteractEntityEvent e){
+        if (e.getHand() != EquipmentSlot.HAND) return;
+        if (!(e.getRightClicked() instanceof Player)) return;
+
+        Player sourcePlayer = e.getPlayer();
+        Player targetPlayer = (Player) e.getRightClicked();
+
+        MenuContext nctx = new MenuContext();
+        nctx.set("target", targetPlayer.getUniqueId());
+
+        MenuRouter.open(sourcePlayer, "transfer");
+    }
+}
