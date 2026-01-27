3ab4c54acc7b3dafc6a7a7ecf67769d828aa09cf
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2026-01-27 15:24:52 +0700

    Add RoleType enum: define roles for governance structure including President, VicePresident, and various member types

---
+++ src/main/java/com/cursebyte/plugin/modules/state/authority/RoleType.java
@@ -0,0 +1,14 @@
+package com.cursebyte.plugin.modules.state.authority;
+
+public enum RoleType {
+    PRESIDENT,
+    VICE_PRESIDENT,
+    BPUN_COORDINATOR,
+    BPUN_MEMBER,
+    BEN_COORDINATOR,
+    BEN_MEMBER,
+    BPN_COORDINATOR,
+    BPN_MEMBER,
+    CITIZEN,
+    NONE
+}
