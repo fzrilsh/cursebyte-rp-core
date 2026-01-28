9a9ea05a231797f34e1b5b9a46a8e757d6f3c6db
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2026-01-28 12:11:46 +0700

    Refactor ReputationRuleEngine: remove commented-out process method and clean up code

--- src/main/java/com/cursebyte/plugin/modules/reputation/ReputationRuleEngine.java
+++ src/main/java/com/cursebyte/plugin/modules/reputation/ReputationRuleEngine.java
@@ -16,51 +16,8 @@ public class ReputationRuleEngine {
         }
 
         double delta = cfg.getDouble(path + ".delta", 0.0);
-
         if(delta == 0.0) return;
 
         ReputationManager.applyDelta(event.getPlayerId(), delta);
     }
-
-    // public static void process(ReputationEvent event) {
-    //     switch (event.getType()) {
-    //         case TRANSFER -> {
-    //             double amount = event.get("amount", Double.class);
-    //             if (amount >= 1000) {
-    //                 ReputationManager.apply(event.getPlayerId(), ReputationAction.TRANSFER_SUCCESS);
-    //             }
-    //         }
-
-    //         case JOB_COMPLETE -> {
-    //             ReputationManager.apply(event.getPlayerId(), ReputationAction.JOB_COMPLETE);
-    //         }
-
-    //         case TAX_PAYMENT -> {
-    //             ReputationManager.apply(event.getPlayerId(), ReputationAction.TAX_PAYMENT);
-    //         }
-
-    //         case PUBLIC_SERVICE -> {
-    //             ReputationManager.apply(event.getPlayerId(), ReputationAction.PUBLIC_SERVICE);
-    //         }
-
-    //         case CRIME -> {
-    //             ReputationManager.apply(event.getPlayerId(), ReputationAction.CRIME);
-    //         }
-
-    //         case SCAM -> {
-    //             ReputationManager.apply(event.getPlayerId(), ReputationAction.SCAM);
-    //         }
-
-    //         case REPORT -> {
-    //             boolean valid = event.get("valid", Boolean.class);
-    //             if (valid) {
-    //                 ReputationManager.apply(event.getPlayerId(), ReputationAction.REPORT_VALID);
-    //             }
-    //         }
-
-    //         case SYSTEM_ABUSE -> {
-    //             ReputationManager.apply(event.getPlayerId(), ReputationAction.ABUSE_SYSTEM);
-    //         }
-    //     }
-    // }
 }
