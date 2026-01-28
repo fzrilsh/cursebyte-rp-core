aba4b81b59e3aa8943b25f139677c513942be185
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2026-01-28 12:25:22 +0700

    Remove unused GovernmentSupplyApi interface

--- src/main/java/com/cursebyte/plugin/modules/government/api/GovernmentSupplyApi.java
+++
@@ -1,9 +0,0 @@
-package com.cursebyte.plugin.modules.government.api;
-
-import java.util.UUID;
-
-import org.bukkit.inventory.ItemStack;
-
-public interface GovernmentSupplyApi {
-    void submitStock(UUID sellerUuid, String category, ItemStack item, int quantity, double unitCost);
-}
